98bb241a87eb50d09019d12bb0ad25628a00b6a4
Chin Perera <chinthanaperera@gmail.com>
2025-10-12 22:33:07 +0100

    feature(database) : Seeds the database

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,34 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "*.supabase.co",
       },
+      {
+        protocol: "https",
+        hostname: "images.ctfassets.net",
+      },
+      {
+        protocol: "https",
+        hostname: "static.grammarly.com",
+      },
+      {
+        protocol: "https",
+        hostname: "ppl-ai-public.s3.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.anthropic.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.notion.so",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn.midjourney.com",
+      },
+      {
+        protocol: "https",
+        hostname: "assets.website-files.com",
+      },
     ],
   },
 };
